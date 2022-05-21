b8f1901b00f5ce564763047bc943fd9465718b88
idaliaFelsmann <idalia.felsmann@gmail.com>
2022-05-21 11:03:31 +0200

    current date and time added, console log away

--- src/index.js
+++ src/index.js
@@ -1,18 +1,49 @@
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureEl = document.querySelector("#degrees");
   let cityEl = document.querySelector(".city");
   let descriptionEl = document.querySelector("#description");
   let humidityEl = document.querySelector("#humidity");
   let windEl = document.querySelector("#wind");
+  let dateEl = document.querySelector(".day");
+  let timeEl = document.querySelector(".time");
 
   temperatureEl.innerHTML = Math.round(response.data.main.temp);
   cityEl.innerHTML = response.data.name;
   descriptionEl.innerHTML = response.data.weather[0].description;
   humidityEl.innerHTML = response.data.main.humidity;
   windEl.innerHTML = Math.round(response.data.wind.speed);
+  dateEl.innerHTML = formatDate(response.data.dt * 1000);
+  timeEl.innerHTML = formatTime(response.data.dt * 1000);
 }
 
 let apiKey = "3f40d3adb734ab9c238d75c205df7d42";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}`;
+}
+
+function formatTime(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
