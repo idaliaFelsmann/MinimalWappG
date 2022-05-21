c20b268f4c9517bf7d793a1e32721dee7a1b45d5
idaliaFelsmann <idalia.felsmann@gmail.com>
2022-05-21 11:46:20 +0200

    botton design adjustment

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function displayTemperature(response) {
   let dateEl = document.querySelector(".day");
   let timeEl = document.querySelector(".time");
 
-  temperatureEl.innerHTML = Math.round(response.data.main.temp);
+  temperatureEl.innerHTML = Math.round(response.data.main.temp) + "°";
   cityEl.innerHTML = response.data.name;
   descriptionEl.innerHTML = response.data.weather[0].description;
   humidityEl.innerHTML = response.data.main.humidity;
@@ -17,7 +17,8 @@ function displayTemperature(response) {
 }
 
 let apiKey = "3f40d3adb734ab9c238d75c205df7d42";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "Berlin";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
 
 function formatDate(timestamp) {
